c7e4b3833b0eb431d802877f5a06c76792c44465
batistadaniel <batistanegobiz@gmail.com>
2026-09-11 10:05:37 -0300

    RF003 - Gestão de orçamentos - adição de quantidade em estoque e unidade de medida

---
+++ database/ddl/rf-003-gestao-orcamento.sql
@@ -0,0 +1,9 @@
+ALTER TABLE produtos
+ADD COLUMN quantidade_estoque NUMERIC(12,3) NOT NULL DEFAULT 0;
+
+ALTER TABLE produtos
+ADD COLUMN unidade_medida VARCHAR(20) NOT NULL DEFAULT 'UN';
+
+ALTER TABLE produtos
+ADD CONSTRAINT chk_estoque_positivo
+CHECK (quantidade_estoque >= 0);
