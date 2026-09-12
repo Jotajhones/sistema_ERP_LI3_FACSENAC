57ce15831cd4560008ec0fe3fe8b0c176eea9f4c
Pedro Henrique <meteusfolha@gmail.com>
2026-09-12 17:08:34 -0300

    RF-002 - integração do logout do frontend com a API

--- src/rf-002-catalogo-produtos/frontend/header/header.js
+++ src/rf-002-catalogo-produtos/frontend/header/header.js
@@ -1,4 +1,4 @@
-
+import { erpFetch } from "../scripts/authInterceptor.js";
 export function renderHeader(activeRoute = 'produtos') {
     const headerContainer = document.getElementById('app-header');
     
@@ -38,11 +38,29 @@ export function renderHeader(activeRoute = 'produtos') {
         document.getElementById('header-user-role').textContent = userRole.toUpperCase();
     }
 
-    document.getElementById('btn-logout').addEventListener('click', () => {
+    document.getElementById('btn-logout').addEventListener('click', async () => {
+
+    const token = localStorage.getItem('authToken');
+
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), 2000);
+
+    try {
+        if (token) {
+            await erpFetch('/auth/logout', {
+                method: 'POST',
+                signal: controller.signal
+            });
+        }
+    } catch (error) {
+        console.error('Erro ao encerrar sessão no servidor:', error);
+    } finally {
+        clearTimeout(timeout);
 
         localStorage.removeItem('authToken');
         localStorage.removeItem('userRole');
-        
+
         window.location.href = '../../../rf-001-gestao-identidade/frontend/login/index.html';
-    });
+    }
+});
 }
